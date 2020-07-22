dd34bccdc2f7cf1ba05144bc4419b4a99713f761
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-07-22 00:49:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,34 @@
-def square_array(array)
-  # your code here
+
+def 
+  
+  square_array(array)
+  
+                          # your code here
+  
+  squared_numbers = New.array
+  
+  count = 0
+  
+  
+  array.each do |number|
+    
+    square = (number * number)
+    
+    squared_numbers[count] << square
+    
+    count =+
+  
+              end
+              
+  squared_numbers
+
 end
+
+
+numbers_list =
+[
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10  
+]
+
+square_array(numnbers_list)
+
